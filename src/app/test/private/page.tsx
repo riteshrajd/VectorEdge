41721fee47ced2c829480a37bfff192f68657ead
Riteshxgit <riteshrajdwivedi@gmail.com>
2025-08-20 19:02:51 +0530

    fix build issues

--- src/app/test/private/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PrivatePage() {
-  return (
-    <div>
-      <h1>Hello World - Private Page</h1>
-      <p>This is a protected page for testing. Only visible if authenticated.</p>
-    </div>
-  );
-}
